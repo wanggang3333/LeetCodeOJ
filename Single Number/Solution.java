a8f3db782d02e326861fcb814f4ebe411ddbb70e
wanggang <wanggang3333@gmail.com>
2015-09-19 13:04:43 +0800

    done the problem136:Single number

---
+++ Single Number/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int sum = 0 ;
+        for(int num:nums){
+            sum = sum ^ num;
+        }
+        
+        return sum;
+    }
+}
