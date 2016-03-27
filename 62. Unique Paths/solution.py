ec46692e0ff2166263aafc1c52f208af13a4644e
wanggang <wanggang3333@gmail.com>
2016-03-27 15:46:41 +0800

    python for the 62 problem
    use arrays palce the number of solutions, array[i,j] = array[i-1,j] +
    array[i,j-1]

---
+++ 62. Unique Paths/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        if m == 1 or n == 1:
+            return 1
+        item = [1] * m
+        array = item * n
+        for i in range(n):
+            array[i] = [1]*m
+        for i in range(1,n):
+            for j in range(1,m):
+                array[i][j] = array[i-1][j] + array[i][j-1]
+        return array[n-1][m-1]
