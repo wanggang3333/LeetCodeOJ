95ccd60a7add2899fcf9ceb652f87689ca8f1f21
wanggang <wanggang3333@gmail.com>
2016-04-21 09:36:44 +0800

    modified the 62 problem

--- 62. Unique Paths/solution.py
+++ 62. Unique Paths/solution.py
@@ -8,9 +8,7 @@ class Solution(object):
         if m == 1 or n == 1:
             return 1
         item = [1] * m
-        array = item * n
-        for i in range(n):
-            array[i] = [1]*m
+        array = [item] * n
         for i in range(1,n):
             for j in range(1,m):
                 array[i][j] = array[i-1][j] + array[i][j-1]
