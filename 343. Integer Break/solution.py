ff2ab5c6329689530ecdd8e6260afadad4627a34
wanggang <wanggang3333@gmail.com>
2016-04-22 22:07:17 +0800

    python for the 343 problem

---
+++ 343. Integer Break/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def integerBreak(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n < 4: return n-1
+        dp = [1] * (n+1)
+        dp[2] = 2
+        dp[3] = 3
+        for i in range(4,n+1):
+            dp[i] = max(dp[i-2] * 2, dp[i-3] * 3)
+        return dp[n]
